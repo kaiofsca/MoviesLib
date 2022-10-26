32d87a5e0f05b8ed609f787ca0355277955e0e39
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-10-26 15:42:31 -0300

    Metade do projeto (requisição a api feita)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { Outlet } from 'react-router-dom'
+
+import NavBar from './components/NavBar'
 
 import './App.css'
 
@@ -5,7 +8,8 @@ function App() {
 
   return (
     <div className="App">
-        <h2>Movies Lib</h2>
+      <NavBar />
+      <Outlet />
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,35 @@
+import { useState, useEffect } from "react"  // relembrando(state gerencia o estado dos filhos e effect para carregar os dados da api quando o site carregar)
+import MovieCard from "../components/MovieCard"
+
+const moviesURL = import.meta.env.VITE_API
+const apiKey = import.meta.env.VITE_API_KEY
+
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+  }
+
+  useEffect(() => {
+
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+
+    getTopRatedMovies(topRatedUrl)
+  }, [])
+  
   return (
-    <div>Home</div>
+    <div className="Container">
+      <h2 className="tittle">Melhores Filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
   )
 }
 
