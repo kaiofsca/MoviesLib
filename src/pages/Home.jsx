d87d2be1bbb6d49a8d12b3f7b612493a50502c65
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-10-27 15:31:18 -0300

    projeto finalizado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react"  // relembrando(state gerencia o estado dos filhos e effect para carregar os dados da api quando o site carregar)
 import MovieCard from "../components/MovieCard"
 
+import './MoviesGrid.css'
+
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
 
@@ -23,8 +25,8 @@ const Home = () => {
   }, [])
   
   return (
-    <div className="Container">
-      <h2 className="tittle">Melhores Filmes:</h2>
+    <div className="container">
+      <h2 className="title">Best Movies:</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
         {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
